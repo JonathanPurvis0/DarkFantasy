2cdf2e762e7fde7872948ad0b631bd64589d56b7
JonathanPurvis0 <jpurvis32@ivytech.edu>
2025-07-01 12:52:26 -0400

    Update main.py
    Added an about tab
    Added layout for 3 chapters

--- main.py
+++ main.py
@@ -2,20 +2,31 @@ def main(): # The beginnging of the program
     print("Welcome to the story!\n" 
     "Please select what you want to do:\n" 
     "1. Start a new adventure\n"
-    "2. Quit\n")
+    "2. About the game\n"
+    "3. Quit\n")
     choice = input("Enter your choice (1 or 2): ") #Get user input
 
     if choice == '1': # If the user chooses to start a new adventure
-        start_adventure()
-    elif choice == '2': # If the user chooses to quit
+        chapterOne()
+    elif choice == '2': # If the user chooses to learn about the game
+        print("About the Game")
+    elif choice == '3': # If the user chooses to quit
         print("Thank you for visiting! Goodbye!")
     else: # If the user enters an invalid choice
         print("Invalid choice. Please try again.")
         main()
 
-def start_adventure(): # Function to start the adventure
+def chapterOne(): # Function to start the adventure
     print("Starting your adventure...\n")
     # Here you can add more code to continue the adventure
 
+def chapterTwo():
+    # Function for the second chapter of the adventure
+    print("Chapter Two Text")
+
+def chapterThree():
+    # Function for the third chapter of the adventure
+    print("Chapter Three Text")
+    
 
 main()
